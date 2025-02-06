533c8d32e3a1e8b782cf83d9db3c0a0498181995
MizanRbf <mizanrbf@gmail.com>
2025-02-06 13:18:38 +0600

    task-5 done

--- Task-5/first.js
+++ Task-5/first.js
@@ -1,1 +1,12 @@
 // Ternary-Number
+const num1 = 50;
+const num2 = 20;
+
+if (num1 > num2) {
+   const result = num1 * 2;
+   console.log(result);
+}
+else {
+   result = num1 + num2
+   console.log(result);
+}
