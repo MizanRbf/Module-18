6bde81ae132dec73787cb243053fcc6d0ac00e8a
MizanRbf <mizanrbf@gmail.com>
2025-02-06 17:03:51 +0600

    updated

--- Task-5/first.js
+++ Task-5/first.js
@@ -1,12 +1,20 @@
 // Ternary-Number
-const num1 = 50;
+const num1 = 40;
 const num2 = 20;
+const result = num1 * 2;
+const result2 = num1 + num2
 
-if (num1 > num2) {
-   const result = num1 * 2;
-   console.log(result);
-}
-else {
-   result = num1 + num2
-   console.log(result);
-}
+// normal if-else
+
+// if (num1 > num2) {
+//    const result = num1 * 2;
+//    console.log(result);
+// }
+// else {
+//    result = num1 + num2
+//    console.log(result);
+// }
+
+// simple ternary
+num1 > num2 ? console.log(result) : console.log(result2);
+
