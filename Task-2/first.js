34348d1a21f89373bc6603175bde432fd4c9eec8
MizanRbf <mizanrbf@gmail.com>
2025-02-06 17:19:15 +0600

    ternary added

--- Task-2/first.js
+++ Task-2/first.js
@@ -1,16 +1,19 @@
 // BMI Calculator and Health Category
 
-const BMI = 40;
+const BMI = 16;
 
-if (BMI < 18.5) {
-  console.log('you are underweight')
-}
-else if (BMI >= 18.5 && BMI <= 24.9) {
-  console.log('you are normal')
-}
-else if (BMI >= 25 && BMI <= 29.9) {
-  console.log('you are overweight')
-}
-else (
-  console.log('you are obese')
-)
+// if (BMI < 18.5) {
+//   console.log('you are underweight')
+// }
+// else if (BMI >= 18.5 && BMI <= 24.9) {
+//   console.log('you are normal')
+// }
+// else if (BMI >= 25 && BMI <= 29.9) {
+//   console.log('you are overweight')
+// }
+// else (
+//   console.log('you are obese')
+// )
+
+
+BMI < 18.5 ? console.log('you are underweight') : BMI >= 18.5 && BMI <= 24.9 ? console.log('you are normal') : BMI >= 25 && BMI <= 29.9 ? console.log('you are overweight') : console.log('you are obese')
