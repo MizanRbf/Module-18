acac08d66717545bcc4072179958f04154049f9f
MizanRbf <mizanrbf@gmail.com>
2025-02-06 17:07:09 +0600

    ternary added

--- Task-1/first.js
+++ Task-1/first.js
@@ -1,9 +1,13 @@
-const burger = 400;
+const burger = 500;
 const coke = '30 tk';
 
-if (burger >= 500){
-  console.log('you will get coke for free')
-}
-else (
-  console.log(coke)
-)
+// if (burger >= 500){
+//   console.log('you will get coke for free')
+// }
+// else (
+//   console.log(coke)
+// )
+
+// simple ternary
+
+burger >= 500 ? console.log('you will get coke for free') : console.log(coke)
